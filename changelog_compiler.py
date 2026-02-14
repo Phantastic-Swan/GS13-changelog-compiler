c1d70bf5a2f5696811521e1b8043e5124f25fd53
Phantastic-Swan <absfree2137@gmail.com>
2026-02-14 16:24:34 +0100

    updated input arguments and makes sure the input file is a yml file

--- changelog_compiler.py
+++ changelog_compiler.py
@@ -39,7 +39,6 @@ def print_changelog(print_dates: bool = False,
 
             for changes_dictionary in yaml_file[date][author]:
                 changes = changes_dictionary.values()
-                # changelog[printed_date][printed_name] = list()
                 for item in changes:
                     changelog[printed_date][printed_name].append(item)
     
@@ -64,29 +63,30 @@ def main():
         description="This program is meant to compile GS13 changelogs \
         into easily copy-able format for news/update announcements"
     )
-    parser.add_argument("input_file", type=valid_file)
+    parser.add_argument("input_file", type=valid_file,
+                        help = "The path to the original .yml changelog file.")
     parser.add_argument("-d", "--print-dates", action="store_true", 
                         help="Whether the changes should be sorted by date.")
     parser.add_argument("-n", "--print-names", action="store_true",
                         help="Whether the changes should be sorted by author.")
     parser.add_argument("-o", "--output-file-name", default="Changelog.txt",
-                        help="The name of the output file.")
-    parser.add_argument("-i", "--input-path", default=".",
-                        help = "The path to the directory where the input file is located. Can be relative or absolute.")
+                        help="The name of the output file. Can be a path to a folder too.")
 
     args = parser.parse_args()
-
-    full_input_path = os.path.join(args.input_path, args.input_file)
 
     # print("Input file:", full_input_path)
     # print("Sort by dates:", args.print_dates)
     # print("Sort by names:", args.print_names)
     # print("Output file:", args.output_file_name)
 
-    input_path: str = full_input_path
+    input_path: str = args.input_file
     print_dates: bool = args.print_dates
     print_names: bool = args.print_names
     output_file_name: str = args.output_file_name
+
+    if not input_path.endswith(".yaml") and not input_path.endswith(".yml"):
+        print("File is not in YAML/YML format!")
+        return    
 
     print_changelog(print_dates, print_names, input_path, output_file_name)
 
